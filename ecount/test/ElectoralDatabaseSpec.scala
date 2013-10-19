37bea31e9f6da923d0026a8ba1c631755a2d327a
John Strong <strngj411@gmail.com>
2013-10-19 14:59:10 +0100

    added runner class for database unit tests

---
+++ ecount/test/ElectoralDatabaseSpec.scala
@@ -0,0 +1,18 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: User 1
+ * Date: 19/10/13
+ * Time: 14:53
+ * To change this template use File | Settings | File Templates.
+ */
+import org.specs2.mutable._
+import org.specs2.runner._
+import org.junit.runner._
+
+import play.api.test._
+import play.api.test.Helpers._
+
+// contains units tests for Postgres Database queries
+class ElectoralDatabaseSpec extends Specification {
+
+}
